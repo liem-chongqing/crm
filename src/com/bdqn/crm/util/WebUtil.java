9287db291c4248b91474a348b878a5a9f06d3bf4
liem <996052600@qq.com>
2019-01-03 08:44:59 +0800

    crm

--- src/com/bdqn/crm/util/WebUtil.java
+++ src/com/bdqn/crm/util/WebUtil.java
@@ -1,36 +1,107 @@
 package com.bdqn.crm.util;
 
-
-
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Method;
+import java.io.PrintWriter;
 
-public class WebUtil extends HttpServlet {
+public class WebUtil {
 
-    public final static String PATH = "/WEB-INF/pages/";
-    public final static String COMMAND = "command";
-    public final static String SUFFIX = ".jsp";
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp){
-        this.doGet(req, resp);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
-        String command = req.getParameter(COMMAND);
+    /**
+     * 设置json返回数据
+     * @return
+     */
+    public static void setJsonResponse(HttpServletResponse response, String jsonStr){
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        PrintWriter out = null;
         try {
-            Class clazz = this.getClass();
-            Method method = clazz.getMethod(command, HttpServletRequest.class,HttpServletResponse.class);
-            String path = (String) method.invoke(this, req, resp);
-            if(path!=null && !"".equals(path)) {
-                req.getRequestDispatcher(PATH.concat(path).concat(SUFFIX)).forward(req, resp);
-                return;
-            }
+            out = response.getWriter();
+            out.append(jsonStr);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("设置json返回数据失败:"+e.getMessage());
+        } finally {
+            if (out != null) {
+                out.close();
+            }
         }
     }
+
+    /**
+     * 获取全部url
+     * @param request
+     * @return
+     */
+    public static String getRequestURLAll(HttpServletRequest request){
+        if (request == null) {
+            return "";
+        }
+        return request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort() + request.getContextPath() + getRequestURL(request);
+    }
+
+    /**
+     * 获取应用访问url
+     * @param request
+     * @return
+     */
+    public static String getRequestWebUrl(HttpServletRequest request){
+        return request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort() + request.getContextPath();
+    }
+
+    /**
+     * 获取url
+     * @param request
+     * @return
+     */
+    public static String getRequestURL(HttpServletRequest request) {
+        if (request == null) {
+            return "";
+        }
+        String url = "";
+        url = request.getServletPath();
+
+        if (!"".equals(request.getQueryString()) && request.getQueryString() != null) {
+            url = url + "?" + request.getQueryString();
+        }
+        return url;
+    }
+
+    /**
+     * 获取用户真实IP地址，不使用request.getRemoteAddr();的原因是有可能用户使用了代理软件方式避免真实IP地址,
+     *
+     * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值，究竟哪个才是真正的用户端的真实IP呢？
+     * 答案是取X-Forwarded-For中第一个非unknown的有效IP字符串。
+     *
+     * 如：X-Forwarded-For：192.168.1.110, 192.168.1.120, 192.168.1.130,
+     * 192.168.1.100
+     *
+     * 用户真实IP为： 192.168.1.110
+     *
+     * @param request
+     * @return
+     */
+    public static String getIpAddress(HttpServletRequest request) {
+        try {
+            String unknown = "unknown";
+            String ip = request.getHeader("x-forwarded-for");
+            if (ip == null || ip.length() == 0 || unknown.equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || unknown.equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ip == null || ip.length() == 0 || unknown.equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (ip == null || ip.length() == 0 || unknown.equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (ip == null || ip.length() == 0 || unknown.equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+            }
+            return ip;
+        }catch (Exception e) {
+            return "";
+        }
+    }
+
 }
